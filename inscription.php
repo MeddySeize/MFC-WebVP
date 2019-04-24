a1c207da15189c75d14cbbf0141d8fa85806e5d1
MeddySeize <meddy.seize@gmail.com>
2019-04-24 11:18:16 +0200

    Avancemenet

--- inscription.php
+++ inscription.php
@@ -1,6 +1,7 @@
 <?php
 session_start()
 
+
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -10,6 +11,8 @@ session_start()
   <link rel="stylesheet" href="form.css">
 </head>
 <body>
+<?php $bdd = new PDO('mysql:host=localhost;dbname=webvp2', 'root', ''); ?>
+
   <!-- Ce formulaire permet à l'utilisateur de s'inscrire -->
 <form action="inscription.php" method="POST">
     <div class="container">
@@ -37,8 +40,27 @@ session_start()
       <input type="password" placeholder="Obligatoire " name="VerifMdp" required>
       <br>
       <br>
-      <button type="submit"class="validebtn">Valider</button>
+      <button type="submit" name="envoi" class="validebtn">Valider</button>
     </div>
+
+    <?php
+    //Indication de la base de donnée à modifier
+    if (isset($_POST['envoi']) AND 
+			isset($_POST['Nom']) AND !empty($_POST['Nom']) AND 
+			isset($_POST['Prenom']) AND !empty($_POST['']) AND
+      isset($_POST['confmdp']) AND $_POST['confmdp'] == $_POST['mdp'])
+      
+    $req = $bdd->query('select * from utilisateur where nomUti = "'.$_POST['nom'].'"')->rowCount();
+
+    if ($req == 0)
+			{
+				$sent = 'insert into utilisateur(nomUti, prenomUti, typeUti, identificationUti, Mdp) values(\''.$_POST['login'].'\',\''.$_POST['mdp'].'\',0)';
+				$bdd->exec($sent);
+		?>
+    
+			
+		?>
+
 </div>
 </body>
 </html>

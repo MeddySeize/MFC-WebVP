da490a1868c4ccb94121ee09a7780f284e17dae5
MeddySeize <meddy.seize@gmail.com>
2019-05-19 14:55:08 +0200

    test

--- index.php
+++ index.php
@@ -12,22 +12,56 @@
     <script type="text/javascript" src="js/jquery-341.min.js"></script>
     <script src="js/bootstrap-431.min"></script>
     <title>Accueil WebVp</title>
-    <script> 
-    jQuery.noConflict
-    $(document).ready(function(){
-  $("button").click(function(){
-    $("div").animate({left: '250px'});
+    <script>
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
   });
 });
-</script> 
-</head>
-    
+</script>
 <body>
-    <button>Start Animation</button>
 
-<p>By default, all HTML elements have a static position, and cannot be moved. To manipulate the position, remember to first set the CSS position property of the element to relative, fixed, or absolute!</p>
+<div class="container">
+  <h2>Modal Login Example</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-default btn-lg" id="myBtn">Login</button>
 
-<div style="background:#98bf21;height:100px;width:100px;position:absolute;"></div>
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header" style="padding:35px 50px;">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+        </div>
+        <div class="modal-body" style="padding:40px 50px;">
+          <form role="form">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+              <input type="text" class="form-control" id="usrname" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <input type="text" class="form-control" id="psw" placeholder="Enter password">
+            </div>
+            <div class="checkbox">
+              <label><input type="checkbox" value="" checked>Remember me</label>
+            </div>
+              <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+          <p>Not a member? <a href="#">Sign Up</a></p>
+          <p>Forgot <a href="#">Password?</a></p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 
 </body>

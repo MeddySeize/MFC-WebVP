50ccfd71aa7427fa4b637daf15a05f65005f2169
MeddySeize <meddy.seize@gmail.com>
2019-05-20 15:12:48 +0200

    Changed Bootstrap and jquery version

--- index.php
+++ index.php
@@ -5,27 +5,27 @@
     <!--Pour le responsive design-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--declaration des fichiers style: Bootstrap et fontawesome-->
-    <link rel="stylesheet" href="bootstrap-4.3.1/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-337.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="fontawesome-free-5.8.2-web/css/all.css">
     <!--declaration des fichiers javascript: JQUERY et bootstrap js-->
-    <script type="text/javascript" src="js/jquery-341.min.js"></script>
-    <script src="bootstrap-4.3.1/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
     <title>Accueil WebVp</title>
     
 </head>
 <body>
-   <div id="top"><!-- top begin -->
-      <div class="container"><!--container begin-->
-          
-          <div class="col-md-6 offre"><!--col md 6 offre begin-->
-              <a href="#" class="btn btn-success btn-sm">Bienvenue</a>
+   <div id="top"><!-- top debut -->
+      <div class="container"><!--container debut-->
+        
+          <div class="col-md-6 offre"><!--col md 6 offre debut-->
+              <a href="#" class="btn btn-primary btn-sm">Bienvenue</a>
               <a href="Profil.php">4 Items dans votre Panier | Prix Total du Panier: 300€ </a>
-          </div><!--col md 6 offre Finish-->
-          
-          <div class="col-md-6"><!--col md 6 begin-->
-              
-              <ul class="menu"><!--cmenu begin-->
+          </div><!--col md 6 offre debut-->
+        
+          <div class="col-md-6"><!--col md 6 debut-->
+            
+              <ul class="menu"><!--cmenu debut-->
                   <li>
                       <a href="inscription.php">Incription</a>
                   </li>
@@ -38,15 +38,29 @@
                   <li>
                       <a href="Profil.php">Se connecter</a>
                   </li>
-              </ul><!--cmenu Finish-->
-              
-          </div><!--col md 6 Finish-->
-          
-      </div><!--container Finish-->
-       
-   </div><!--top Finish-->
+              </ul><!--cmenu fin -->
+            
+          </div><!--col md 6 fin-->
+        
+      </div><!--container fin-->
+    
+   </div><!--top fin-->
 
- 
+ <div id="navbar" class="navbar navbar-default"><!--navbar debut-->
+
+   <div id="container"><!-- navbar container debut-->
+
+   <div class="navbar-header"><!-- navbar container header debut-->
+   <a href="index.php" class="navbar-brand home"></a>
+
+
+   </div><!-- navbar container header fin-->
+
+
+   </div><!-- navbar container fin-->
+
+
+ </div><!--navbar fin-->
 
 
 </body>

c1ec60376dcb06b6d45d1664f42e2783130e5c84
meddy <meddy.seize@gmail.com>
2019-05-25 00:21:20 +0200

    Changer la bdd

--- webvp2.sql
+++ webvp2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 24 mai 2019 à 10:35
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Généré le :  ven. 24 mai 2019 à 22:14
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `nomCat` varchar(25) COLLATE utf8_bin NOT NULL,
   `description` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `categorie`
@@ -45,7 +45,8 @@ INSERT INTO `categorie` (`id_categorie`, `nomCat`, `description`) VALUES
 (2, 'Projet', 'Vous voulez devenir chef de projet? Ces formations sont pour vous.'),
 (3, 'Infrastructure', 'L\'architecture des SI vous parle? '),
 (4, 'Réseau', 'Apprenez tout sur le réseau informatique et sa sécurité.'),
-(5, 'Développement', 'Apprenez à créer des des Base de données, des applications et sites Web.');
+(5, 'Développement', 'Apprenez à créer des des Base de données, des applications et sites Web.'),
+(6, 'Bureautique', '');
 
 -- --------------------------------------------------------
 
@@ -72,8 +73,8 @@ CREATE TABLE IF NOT EXISTS `commande` (
 DROP TABLE IF EXISTS `formation`;
 CREATE TABLE IF NOT EXISTS `formation` (
   `id_formation` int(11) NOT NULL AUTO_INCREMENT,
-  `nomForma` varchar(50) COLLATE utf8_bin NOT NULL,
-  `descriptionForma` varchar(150) COLLATE utf8_bin DEFAULT NULL,
+  `nomForma` varchar(100) COLLATE utf8_bin NOT NULL,
+  `descriptionForma` varchar(350) COLLATE utf8_bin DEFAULT NULL,
   `prixForma` int(15) NOT NULL,
   `lieuxForma` varchar(50) COLLATE utf8_bin NOT NULL,
   `valideForma` int(1) NOT NULL COMMENT '0 : doit être validé ou supprimé par un admin, 1 : validé et affiché',
@@ -81,17 +82,19 @@ CREATE TABLE IF NOT EXISTS `formation` (
   `id_categorie` int(11) NOT NULL,
   PRIMARY KEY (`id_formation`),
   KEY `FK_catégorie` (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `formation`
 --
 
 INSERT INTO `formation` (`id_formation`, `nomForma`, `descriptionForma`, `prixForma`, `lieuxForma`, `valideForma`, `dureeForma`, `id_categorie`) VALUES
-(2, 'TCP/IP', NULL, 1600, 'Lille', 1, 3, 4),
-(3, 'La technologie Sans-fil WIFI', NULL, 980, 'Lyon', 1, 2, 4),
+(2, 'TCP/IP', 'Vous n’y connaissez rien aux réseaux ? Pas de soucis, ce cours est fait pour tous !', 1600, 'Lille', 1, 3, 4),
+(3, 'La technologie Sans-fil WIFI', 'Désormais, le WiFi s’installe dans toutes les entreprises modernes. Son usage en tout lieu, à tout moment et avec tout type de terminaux et périphériques, nécessite cependant des précautions particulières en termes de sécurité et de performance qui sont indispensables au bon fonctionnement des applications.', 980, 'Lyon', 1, 2, 4),
 (4, 'Voix et Téléphone sur IP', NULL, 3500, 'Nice', 1, 5, 4),
-(5, 'Programmez en PHP 5', NULL, 1800, 'Nantes', 1, 3, 5);
+(5, 'Programmez en PHP 5', 'Si vous voulez apprendre à créer vous aussi des sites web dynamiques, c\'est votre jour de chance : vous êtes sur un cours pour vrais débutants en PHP !', 1800, 'Nantes', 1, 3, 5),
+(6, 'Développez vos Applications avec Android', 'Vous êtes prêts à conquérir le monde Android avec votre première application ? Alors, inscrivez-vous !', 2200, 'Toulouse', 1, 4, 5),
+(7, 'Créer des sites et Applications web avec HTML5 et CSS3', 'Vous souhaitez créer vos propres sites web ? Vous êtes au bon endroit ! Dans ce cours, vous apprendrez comment utiliser HTML5 et CSS3, les deux langages de programmation à la base de tous les sites web.', 2700, 'Nice', 1, 5, 5);
 
 -- --------------------------------------------------------
 
